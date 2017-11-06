bbcb29da71fd9f764077c61d3a6b850b578272f7
Yar-ua <yar.ua.1985@gmail.com>
2017-11-06 18:41:29 +0200

    add ajax to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,11 +31,10 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     # метод корзины add_product описан в модели корзины Cart
     @line_item = @cart.add_product(product.id)
-    update_cart
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Блюдо добавлено в предзаказ (корзину)' }
-        format.json { render :show, status: :created, location: @line_item }
+        format.js
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
