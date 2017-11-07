c98f7700ec53415dd94f10dfac9421768f0b6b9e
Yar-ua <yar.ua.1985@gmail.com>
2017-11-07 15:05:09 +0200

    add RedisCloud settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,10 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  # for production
+  config.action_cable.allowed_request_origins = [ 'https://rails-restaurant.herokuapp.com', 'http://rails-restaurant.herokuapp.com' ]
+  config.action_cable.url = 'wss://sitepoint-actioncable.herokuapp.com/cable'
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 
   # просмотр админом оформленных заказов
   resources :single_orders, only: [:index, :show, :destroy]
