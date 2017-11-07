7a726c779c3239ee592249d06a8b6d3225761040
Yar-ua <yar.ua.1985@gmail.com>
2017-11-07 14:46:20 +0200

    update. ready to deploy

--- test/controllers/group_line_items_controller_test.rb
+++ test/controllers/group_line_items_controller_test.rb
@@ -5,44 +5,4 @@ class GroupLineItemsControllerTest < ActionDispatch::IntegrationTest
     @group_line_item = group_line_items(:one)
   end
 
-  test "should get index" do
-    get group_line_items_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_group_line_item_url
-    assert_response :success
-  end
-
-  test "should create group_line_item" do
-    assert_difference('GroupLineItem.count') do
-      post group_line_items_url, params: { group_line_item: { group_cart_id: @group_line_item.group_cart_id, product_id: @group_line_item.product_id } }
-    end
-
-    assert_redirected_to group_line_item_url(GroupLineItem.last)
-  end
-
-  test "should show group_line_item" do
-    get group_line_item_url(@group_line_item)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_group_line_item_url(@group_line_item)
-    assert_response :success
-  end
-
-  test "should update group_line_item" do
-    patch group_line_item_url(@group_line_item), params: { group_line_item: { group_cart_id: @group_line_item.group_cart_id, product_id: @group_line_item.product_id } }
-    assert_redirected_to group_line_item_url(@group_line_item)
-  end
-
-  test "should destroy group_line_item" do
-    assert_difference('GroupLineItem.count', -1) do
-      delete group_line_item_url(@group_line_item)
-    end
-
-    assert_redirected_to group_line_items_url
-  end
 end
--- test/controllers/group_orders_admin_controller_test.rb
+++ test/controllers/group_orders_admin_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class GroupOrdersAdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get dish" do
-    get group_orders_admin_dish_url
-    assert_response :success
-  end
+
 
 end
